2e0268c7b70ff25792abd101826a87f7bdb32859
pghemanth <pghemanthpg@gmail.com>
2024-01-05 16:06:40 +0530

    new

---
+++ praject2.py
@@ -0,0 +1,32 @@
+
+prodect=input('select your prodect:-')
+quantity=int(input('select of quantity:-'))    
+option=input('plaece select you prodect 1-->tv\n2-->mobile\n3-->laptap:-')
+match option:
+    case 1:
+        
+        
+       
+
+
+    def __init__(self,name,mobile,location,total_quantity,totaol_price):
+        self.name=name
+        self.mobile=mobile
+        self.location=location
+        self.total_quantity=total_quantity
+        self.total_price=total_price
+
+
+
+
+
+
+
+
+     
+prodect=input('select your prodect:-')
+quantity=int(input('select of quantity:-'))    
+total_price=quantity['tv','mobileprice
+name=input('enter your name:-')
+mobile=input('enter your mobile number:-')
+location=input('enter tour locatin:-')
